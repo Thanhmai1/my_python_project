7a30592c04cfe68b40d69981e1171d5bb42058e7
Thanhmai1 <141890766+Thanhmai1@users.noreply.github.com>
2024-09-15 21:53:51 +0700

    update doc string

--- ex_practise.py
+++ ex_practise.py
@@ -1,11 +1,11 @@
-def area(height:float, wight:float) -> float:
+def area(height:float, wight:float)-> float:
     """_Caculator Area_
 
     Args:
-        height (_float_): _description_
-        wight (_float_): _description_
+        height (_float_): _height of shape_
+        wight (_float_): _wight of shape_
 
     Returns:
-        _float_: _Value of area rectangle or square_
+        _float_: _Area of rectangle or square_
     """
     return height * wight
