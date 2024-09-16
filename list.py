3d6e73e631ad717ce699c12f2c3f2993beedf489
Thanhmai1 <141890766+Thanhmai1@users.noreply.github.com>
2024-09-16 09:24:00 +0700

    .

---
+++ list.py
@@ -0,0 +1,10 @@
+phone = ['Nokia', 'Samsung', 'Iphone', 'Redmi']
+index = 0
+for i in phone:
+    print('for loop')
+    print(f"Element [i]")
+    
+    
+while index < len(phone):
+    print(f"Element: {phone[index]}")
+    index += 1
