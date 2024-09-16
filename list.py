afbffbb4f69c5e36ac1de71f615231970b5b31ed
Thanhmai1 <141890766+Thanhmai1@users.noreply.github.com>
2024-09-16 09:44:15 +0700

    .

--- list.py
+++ list.py
@@ -1,4 +1,5 @@
 phone = ['Nokia', 'Samsung', 'Iphone', 'Redmi']
+num = []
 index = 0
 for i in phone:
     print('for loop')
@@ -6,5 +7,16 @@ for i in phone:
     
     
 while index < len(phone):
+    print('while loop')
     print(f"Element: {phone[index]}")
     index += 1
+    
+while True:
+    value = input("Enter a value: ")
+    po = int(input("Enter a positi: "))
+    ans = input("Do you want exit [y/n]: ")
+    if ans.lower() == 'y':
+        print('Exitting...')
+        break
+    num.insert(po, value)
+    print(num)
