eff053efa99e321a3df6e445a1f4e521edb21ac7
Thanhmai1 <141890766+Thanhmai1@users.noreply.github.com>
2024-09-16 11:06:56 +0700

    dict

---
+++ dict.py
@@ -0,0 +1,11 @@
+def dict_example():
+    person = {
+        "name": "Nguyen Van A",
+        "age": 20,
+        "address": "Ha Noi"
+    }
+    print(person)
+    print(f"Name: {person['name']}")
+
+if __name__ == "__main__":
+    dict_example()
