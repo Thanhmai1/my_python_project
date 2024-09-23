9db564229a83f11f0c12757cc09da0a8cf28f41d
Thanhmai1 <141890766+Thanhmai1@users.noreply.github.com>
2024-09-23 21:18:03 +0700

    db students

--- python_db/students.py
+++ python_db/students.py
@@ -1,4 +1,5 @@
 import mysql.connector
+
 def connect_to_db():
     return mysql.connector.connect(
         host="localhost",
@@ -6,46 +7,83 @@ def connect_to_db():
         password="",
         database="t2308m"
     )
-
+    
 mydb = connect_to_db()  
-def insert_student(name, age, grade):    
-    cursor = mydb.cursor()
-    sql = "INSERT INTO students (name, age, grade) VALUES (%s, %s, %s)"
-    values = (name, age, grade)
+cursor = mydb.cursor()
+
+def insert_student(name, grade):        
+    sql = "INSERT INTO students (name, grade) VALUES (%s, %s)"
+    values = (name, grade)
     cursor.execute(sql, values)
 
     mydb.commit()
 
     print(cursor.rowcount, 'was inserted')
     
-def get_all_students():
-    cursor = mydb.cursor()
-    cursor.execute("SELECT * FROM students")
-    students = cursor.fetchall()
+def get_all_students():    
+    cursor.execute("SELECT students.name, ratings.rating FROM students INNER JOIN ratings ON students.grade = ratings.grade")
+    students = cursor.fetchall()    
     
     for x in students:
         print(students)
-        
+
+def get_random_students():
+    sql = "SELECT name, grade FROM students ORDER BY RAND() LIMIT 1"
+    cursor.execute(sql)
+    students = cursor.fetchone()      
+    if students:
+        print(students)
+    else:
+        print("None")
+def update_grade_student(name, grade):
+    sql = "UPDATE students SET grade = %s WHERE name = %s"
+    
+    values = (grade, name)
+    cursor.execute(sql, values)
+    
+    mydb.commit()
+    print(cursor.rowcount, 'row were updated')
+
+    
+def delete_student(name):
+    sql = "DELETE FROM students WHERE name = '%s'"
+    values = (name)
+    cursor.execute(sql, values)
+    
+    mydb.commit()
+    print(cursor.rowcount, 'was inserted')
+    
 def main():
     while True:
         print("----Menu----")
         print('1. Insert students')
         print("2. Display all students")
-        print("3. Exit")
+        print("3. Display 1 student")
+        print("4. Delete student")
+        print("5. Update grade student")
+        print("6. Exit")
         print("Enter your choice: ")
         choice = int(input())
         
         if choice == 1:
-            name = input('Enter your name: ')
-            age = int(input('Enter your age: '))
+            name = input('Enter your name: ')            
             grade = int(input("Enter your grade: "))
 
-            insert_student(name, age, grade)
+            insert_student(name, grade)
             
         elif choice == 2:
-            get_all_students()
-
+            get_all_students()        
         elif choice == 3:
+            get_random_students()
+        elif choice == 4:
+            name = input("Enter name to delete: ")
+            delete_student(name)
+        elif choice == 5:
+            name = input("Enter name to update: ")
+            grade = input("Enter grade to update: ")
+            update_grade_student(name, grade)
+        
+        elif choice == 6:
             print("Exitting...")
             break
         
